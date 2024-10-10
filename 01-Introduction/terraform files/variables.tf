a97f05c8e49b5ac62ab547bb16e21d9c5def8de5
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-10-10 10:07:06 +0200

    Add files via upload

---
+++ 01-Introduction/terraform files/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "my_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ansible-key" {
+  type    = string
+  default = "ansible-key"
+}
+
+
+
